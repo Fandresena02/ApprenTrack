c99b7ee9444f4ed4ae09f790af90551ef87eb1c2
steven <steven-van@outlook.fr>
2023-10-13 10:27:16 +0200

    fix: order bdd

--- src/main/java/fr/efrei/apptrack/utils/BDD_projet.sql
+++ src/main/java/fr/efrei/apptrack/utils/BDD_projet.sql
@@ -22,82 +22,84 @@ DROP TABLE IF EXISTS MEMOIRERAPPORT;
 DROP TABLE IF EXISTS EVALUATION;
 DROP TABLE IF EXISTS SOUTENANCE;
 DROP TABLE IF EXISTS ESTEVALUE;
- 
+
 
 CREATE TABLE PERSONNE (
-    idPersonne INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL,
-    prenom VARCHAR(255) NOT NULL,
-    email VARCHAR(255),
-    telephone VARCHAR(15)
-);
-
-CREATE TABLE APPRENTI (
-    anneeAcademique INT,
-    majeure VARCHAR(255),
-    programme VARCHAR(255),
-    idPersonne INT,
-    idEntreprise INT,
-    FOREIGN KEY (idPersonne) REFERENCES PERSONNE(idPersonne),
-    FOREIGN KEY (idEntreprise) REFERENCES ENTREPRISE(idEntreprise)
-);
-
-CREATE TABLE MAITREAPPRENTISSAGE (
-    poste VARCHAR(255),
-    remarques TEXT,
-    idPersonne INT,
-    idEntreprise INT,
-    FOREIGN KEY (idPersonne) REFERENCES PERSONNE(idPersonne),
-    FOREIGN KEY (idEntreprise) REFERENCES ENTREPRISE(idEntreprise)
-);
-
-CREATE TABLE VISITE (
-    dateVisite DATE,
-    format VARCHAR(255),
-    compteRenduExpress TEXT,
-    idPersonne INT,
-    FOREIGN KEY (idPersonne) REFERENCES PERSONNE(idPersonne)
+                          idPersonne INT AUTO_INCREMENT PRIMARY KEY,
+                          nom VARCHAR(255) NOT NULL,
+                          prenom VARCHAR(255) NOT NULL,
+                          email VARCHAR(255),
+                          telephone VARCHAR(15)
 );
 
 CREATE TABLE ENTREPRISE (
-    idEntreprise INT AUTO_INCREMENT PRIMARY KEY,
-    raisonSocial VARCHAR(255) NOT NULL,
-    adresse TEXT,
-    informations TEXT
+                            idEntreprise INT AUTO_INCREMENT PRIMARY KEY,
+                            raisonSocial VARCHAR(255) NOT NULL,
+                            adresse TEXT,
+                            informations TEXT
 );
 
+CREATE TABLE APPRENTI (
+                          anneeAcademique INT,
+                          majeure VARCHAR(255),
+                          programme VARCHAR(255),
+                          idPersonne INT,
+                          idEntreprise INT,
+                          FOREIGN KEY (idPersonne) REFERENCES PERSONNE(idPersonne),
+                          FOREIGN KEY (idEntreprise) REFERENCES ENTREPRISE(idEntreprise)
+);
+
+CREATE TABLE MAITREAPPRENTISSAGE (
+                                     poste VARCHAR(255),
+                                     remarques TEXT,
+                                     idPersonne INT,
+                                     idEntreprise INT,
+                                     FOREIGN KEY (idPersonne) REFERENCES PERSONNE(idPersonne),
+                                     FOREIGN KEY (idEntreprise) REFERENCES ENTREPRISE(idEntreprise)
+);
+
+CREATE TABLE VISITE (
+                        dateVisite DATE,
+                        format VARCHAR(255),
+                        compteRenduExpress TEXT,
+                        idPersonne INT,
+                        FOREIGN KEY (idPersonne) REFERENCES PERSONNE(idPersonne)
+);
+
+
+
 CREATE TABLE MISSION (
-    idMission INT AUTO_INCREMENT PRIMARY KEY,
-    motsCles TEXT,
-    commentaires TEXT,
-    metierCible VARCHAR(255),
-    idPersonne INT,
-    FOREIGN KEY (idPersonne) REFERENCES PERSONNE(idPersonne)
+                         idMission INT AUTO_INCREMENT PRIMARY KEY,
+                         motsCles TEXT,
+                         commentaires TEXT,
+                         metierCible VARCHAR(255),
+                         idPersonne INT,
+                         FOREIGN KEY (idPersonne) REFERENCES PERSONNE(idPersonne)
 );
 
 CREATE TABLE EVALUATION (
-    idEvaluation INT AUTO_INCREMENT PRIMARY KEY,
-    note DECIMAL(3, 1),
-    commentaires TEXT
+                            idEvaluation INT AUTO_INCREMENT PRIMARY KEY,
+                            note DECIMAL(3, 1),
+                            commentaires TEXT
 );
 
 CREATE TABLE MEMOIRERAPPORT (
-    themeSujet TEXT,
-    idEvaluation INT,
-    FOREIGN KEY (idEvaluation) REFERENCES EVALUATION(idEvaluation)
+                                themeSujet TEXT,
+                                idEvaluation INT,
+                                FOREIGN KEY (idEvaluation) REFERENCES EVALUATION(idEvaluation)
 );
 
 CREATE TABLE SOUTENANCE (
-    dateSoutenance DATE,
-    idEvaluation INT,
-    FOREIGN KEY (idEvaluation) REFERENCES EVALUATION(idEvaluation)
+                            dateSoutenance DATE,
+                            idEvaluation INT,
+                            FOREIGN KEY (idEvaluation) REFERENCES EVALUATION(idEvaluation)
 );
 
 CREATE TABLE ESTEVALUE (
-    idEvaluation INT,
-    idPersonne INT,
-    FOREIGN KEY (idEvaluation) REFERENCES EVALUATION(idEvaluation),
-    FOREIGN KEY (idPersonne) REFERENCES PERSONNE(idPersonne)
+                           idEvaluation INT,
+                           idPersonne INT,
+                           FOREIGN KEY (idEvaluation) REFERENCES EVALUATION(idEvaluation),
+                           FOREIGN KEY (idPersonne) REFERENCES PERSONNE(idPersonne)
 );
 
 
@@ -110,6 +112,14 @@ VALUES
     ('Johnson', 'Bob', 'bob.johnson@example.com', '555-123-4567'),
     ('Brown', 'Emily', 'emily.brown@example.com', '222-333-4444'),
     ('Davis', 'Michael', 'michael.davis@example.com', '777-888-9999');
+INSERT INTO ENTREPRISE (raisonSocial, adresse, informations)
+VALUES
+    ('XYZ Tech', '123 Rue de la Technologie, VilleTech', 'Entreprise spécialisée en informatique'),
+    ('BuildPro', '456 Avenue des Bâtisseurs, VilleConstruction', 'Entreprise de génie civil renommée'),
+    ('MarketMax', '789 Boulevard du Marketing, VilleMarketing', 'Agence de marketing prospère'),
+    ('SoftDev', '101 Rue du Développement, VilleDev', 'Société de développement de logiciels'),
+    ('FinanceWorld', '202 Avenue de la Finance, VilleFinance', 'Entreprise financière leader du secteur');
+
 
 INSERT INTO APPRENTI (anneeAcademique, majeure, programme, idPersonne, idEntreprise)
 VALUES
@@ -121,11 +131,11 @@ VALUES
 
 INSERT INTO MAITREAPPRENTISSAGE (poste, remarques, idPersonne, idEntreprise)
 VALUES
-    ('Ingénieur en Chef', 'Excellente supervision', 6, 1),
-    ('Architecte', 'Très compétent', 7, 2),
-    ('Directeur Marketing', 'Très impliqué', 8, 3),
-    ('Chef de Projet', 'Très expérimenté', 9, 4),
-    ('Directeur Financier', 'Très compétent', 10, 5);
+    ('Ingénieur en Chef', 'Excellente supervision', 1, 1),
+    ('Architecte', 'Très compétent', 2, 2),
+    ('Directeur Marketing', 'Très impliqué', 3, 3),
+    ('Chef de Projet', 'Très expérimenté', 4, 4),
+    ('Directeur Financier', 'Très compétent', 5, 5);
 
 INSERT INTO VISITE (dateVisite, format, compteRenduExpress, idPersonne)
 VALUES
@@ -135,13 +145,6 @@ VALUES
     ('2023-08-05', 'En ligne', 'Entretien productif', 4),
     ('2023-09-12', 'En personne', 'Rencontre prometteuse', 5);
 
-INSERT INTO ENTREPRISE (raisonSocial, adresse, informations)
-VALUES
-    ('XYZ Tech', '123 Rue de la Technologie, VilleTech', 'Entreprise spécialisée en informatique'),
-    ('BuildPro', '456 Avenue des Bâtisseurs, VilleConstruction', 'Entreprise de génie civil renommée'),
-    ('MarketMax', '789 Boulevard du Marketing, VilleMarketing', 'Agence de marketing prospère'),
-    ('SoftDev', '101 Rue du Développement, VilleDev', 'Société de développement de logiciels'),
-    ('FinanceWorld', '202 Avenue de la Finance, VilleFinance', 'Entreprise financière leader du secteur');
 
 INSERT INTO MISSION (motsCles, commentaires, metierCible, idPersonne)
 VALUES
@@ -177,8 +180,8 @@ VALUES
 
 INSERT INTO ESTEVALUE (idEvaluation, idPersonne)
 VALUES
-    (1, 1), 
-    (2, 2),  
-    (3, 3), 
-    (4, 4),  
-    (5, 5); 
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5);
