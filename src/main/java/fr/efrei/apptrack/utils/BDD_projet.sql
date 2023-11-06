f7ca6dc4df81e62bcb10b845314f607023a741eb
Fenyx <64859587+steven-van@users.noreply.github.com>
2023-11-07 00:09:47 +0100

    Update BDD_projet.sql

--- src/main/java/fr/efrei/apptrack/utils/BDD_projet.sql
+++ src/main/java/fr/efrei/apptrack/utils/BDD_projet.sql
@@ -1,3 +1,4 @@
+-- OLD
 -- PERSONNE(idPersonne, nom, prenom, email, telephone)
 -- APPRENTI(anneeAcademique, majeure, programme, #idPersonne, #idEntreprise)
 -- MAITREAPPRENTISSAGE(poste, remarques, #idPersonne, #idEntreprise)
@@ -13,7 +14,6 @@ DROP SCHEMA IF EXISTS ApprenTrackBDD;
 CREATE SCHEMA ApprenTrackBDD;
 USE ApprenTrackBDD;
 DROP TABLE IF EXISTS VISITE;
-DROP TABLE IF EXISTS PERSONNE;
 DROP TABLE IF EXISTS APPRENTI;
 DROP TABLE IF EXISTS MAITREAPPRENTISSAGE;
 DROP TABLE IF EXISTS ENTREPRISE;
@@ -23,14 +23,6 @@ DROP TABLE IF EXISTS EVALUATION;
 DROP TABLE IF EXISTS SOUTENANCE;
 DROP TABLE IF EXISTS ESTEVALUE;
 
-
-CREATE TABLE PERSONNE (
-                          idPersonne INT AUTO_INCREMENT PRIMARY KEY,
-                          nom VARCHAR(255) NOT NULL,
-                          prenom VARCHAR(255) NOT NULL,
-                          email VARCHAR(255),
-                          telephone VARCHAR(15)
-);
 
 CREATE TABLE ENTREPRISE (
                             idEntreprise INT AUTO_INCREMENT PRIMARY KEY,
@@ -48,15 +40,17 @@ CREATE TABLE APPRENTI (
                           anneeAcademique INT,
                           majeure VARCHAR(255),
                           programme VARCHAR(255),
-                          isArchive BOOL
+                          isArchive BOOL,
+                          idEntreprise INT,
+                          FOREIGN KEY (idEntreprise) REFERENCES ENTREPRISE(idEntreprise)
+  
 );
 
 CREATE TABLE MAITREAPPRENTISSAGE (
+                                     idMaitreApprentissage INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
                                      poste VARCHAR(255),
                                      remarques TEXT,
-                                     idPersonne INT,
                                      idEntreprise INT,
-                                     FOREIGN KEY (idPersonne) REFERENCES PERSONNE(idPersonne),
                                      FOREIGN KEY (idEntreprise) REFERENCES ENTREPRISE(idEntreprise)
 );
 
@@ -64,8 +58,8 @@ CREATE TABLE VISITE (
                         dateVisite DATE,
                         format VARCHAR(255),
                         compteRenduExpress TEXT,
-                        idPersonne INT,
-                        FOREIGN KEY (idPersonne) REFERENCES PERSONNE(idPersonne)
+                        idApprenti INT,
+                        FOREIGN KEY (idApprenti) REFERENCES APPRENTI(idApprenti)
 );
 
 
@@ -75,8 +69,8 @@ CREATE TABLE MISSION (
                          motsCles TEXT,
                          commentaires TEXT,
                          metierCible VARCHAR(255),
-                         idPersonne INT,
-                         FOREIGN KEY (idPersonne) REFERENCES PERSONNE(idPersonne)
+                         idApprenti INT,
+                         FOREIGN KEY (idApprenti) REFERENCES APPRENTI(idApprenti)
 );
 
 CREATE TABLE EVALUATION (
@@ -99,48 +93,30 @@ CREATE TABLE SOUTENANCE (
 
 CREATE TABLE ESTEVALUE (
                            idEvaluation INT,
-                           idPersonne INT,
+                           idApprenti INT,
                            FOREIGN KEY (idEvaluation) REFERENCES EVALUATION(idEvaluation),
-                           FOREIGN KEY (idPersonne) REFERENCES PERSONNE(idPersonne)
+                           FOREIGN KEY (idApprenti) REFERENCES APPRENTI(idApprenti)
 );
 
 
 
-
-INSERT INTO PERSONNE (nom, prenom, email, telephone)
+INSERT INTO APPRENTI (nom, prenom, email, telephone, anneeAcademique, majeure, programme, isArchive, idEntreprise)
 VALUES
-    ('Doe', 'John', 'john.doe@example.com', '123-456-7890'),
-    ('Smith', 'Alice', 'alice.smith@example.com', '987-654-3210'),
-    ('Johnson', 'Bob', 'bob.johnson@example.com', '555-123-4567'),
-    ('Brown', 'Emily', 'emily.brown@example.com', '222-333-4444'),
-    ('Davis', 'Michael', 'michael.davis@example.com', '777-888-9999');
-INSERT INTO ENTREPRISE (raisonSocial, adresse, informations)
+    ('Doe', 'John', 'john.doe@example.com', '123-456-7890', 2023, 'Informatique', 'BAC+5', false, 1),
+    ('Smith', 'Alice', 'alice.smith@example.com', '987-654-3210', 2022, 'Génie Civil', 'BAC+4', false, 2),
+    ('Johnson', 'Bob', 'bob.johnson@example.com', '555-123-4567', 2023, 'Marketing', 'BAC+5', false, 3),
+    ('Brown', 'Emily', 'emily.brown@example.com', '222-333-4444', 2022, 'Informatique', 'BAC+4', false, 4),
+    ('Davis', 'Michael', 'michael.davis@example.com', '777-888-9999', 2023, 'Finance', 'BAC+5', false, 5);
+
+INSERT INTO MAITREAPPRENTISSAGE (poste, remarques, idEntreprise)
 VALUES
-    ('XYZ Tech', '123 Rue de la Technologie, VilleTech', 'Entreprise spécialisée en informatique'),
-    ('BuildPro', '456 Avenue des Bâtisseurs, VilleConstruction', 'Entreprise de génie civil renommée'),
-    ('MarketMax', '789 Boulevard du Marketing, VilleMarketing', 'Agence de marketing prospère'),
-    ('SoftDev', '101 Rue du Développement, VilleDev', 'Société de développement de logiciels'),
-    ('FinanceWorld', '202 Avenue de la Finance, VilleFinance', 'Entreprise financière leader du secteur');
+    ('Ingénieur en Chef', 'Excellente supervision', 1),
+    ('Architecte', 'Très compétent', 2),
+    ('Directeur Marketing', 'Très impliqué', 3),
+    ('Chef de Projet', 'Très expérimenté', 4),
+    ('Directeur Financier', 'Très compétent', 5);
 
-
-INSERT INTO APPRENTI (idApprenti, nom, prenom, email, telephone, anneeAcademique, majeure, programme, isArchive)
-VALUES
-    (1, 'Doe', 'John', 'john.doe@example.com', '123-456-7890', 2023, 'Informatique', 'BAC+5', false),
-    (2, 'Smith', 'Alice', 'alice.smith@example.com', '987-654-3210', 2022, 'Génie Civil', 'BAC+4', false),
-    (3, 'Johnson', 'Bob', 'bob.johnson@example.com', '555-123-4567', 2023, 'Marketing', 'BAC+5', false),
-    (4, 'Brown', 'Emily', 'emily.brown@example.com', '222-333-4444', 2022, 'Informatique', 'BAC+4', false),
-    (5, 'Davis', 'Michael', 'michael.davis@example.com', '777-888-9999', 2023, 'Finance', 'BAC+5', false),
-    (6, 'Sam', 'Smith', 'sam.smith@example.com', '156-55-1845', 2025, 'LSI', 'BAC+5', false);
-
-INSERT INTO MAITREAPPRENTISSAGE (poste, remarques, idPersonne, idEntreprise)
-VALUES
-    ('Ingénieur en Chef', 'Excellente supervision', 1, 1),
-    ('Architecte', 'Très compétent', 2, 2),
-    ('Directeur Marketing', 'Très impliqué', 3, 3),
-    ('Chef de Projet', 'Très expérimenté', 4, 4),
-    ('Directeur Financier', 'Très compétent', 5, 5);
-
-INSERT INTO VISITE (dateVisite, format, compteRenduExpress, idPersonne)
+INSERT INTO VISITE (dateVisite, format, compteRenduExpress, idApprenti)
 VALUES
     ('2023-05-15', 'En personne', 'Bonne réunion', 1),
     ('2023-06-20', 'En ligne', 'Discussion approfondie', 2),
@@ -148,8 +124,7 @@ VALUES
     ('2023-08-05', 'En ligne', 'Entretien productif', 4),
     ('2023-09-12', 'En personne', 'Rencontre prometteuse', 5);
 
-
-INSERT INTO MISSION (motsCles, commentaires, metierCible, idPersonne)
+INSERT INTO MISSION (motsCles, commentaires, metierCible, idApprenti)
 VALUES
     ('Développement web, Java, Front-end', 'Mission réussie', 'Développeur web', 1),
     ('Conception de bâtiments, Génie civil', 'Mission accomplie', 'Ingénieur civil', 2),
@@ -157,31 +132,7 @@ VALUES
     ('Développement de logiciels, C#, Back-end', 'Mission en cours', 'Développeur logiciel', 4),
     ('Analyse financière, Investissement', 'Mission achevée', 'Analyste financier', 5);
 
-INSERT INTO EVALUATION (note, commentaires)
-VALUES
-    (4.5, 'Excellent travail.'),
-    (3.8, 'Très bon projet.'),
-    (4.0, 'Très satisfaisant.'),
-    (4.2, 'Très bien réalisé.'),
-    (3.5, 'Bonne performance.');
-
-INSERT INTO MEMOIRERAPPORT (themeSujet, idEvaluation)
-VALUES
-    ('Analyse des performances web', 1),
-    ('Conception de bâtiments durables', 2),
-    ('Stratégie marketing pour les produits technologiques', 3),
-    ('Développement d une application de gestion', 4),
-    ('Analyse financière des investissements', 5);
-
-INSERT INTO SOUTENANCE (dateSoutenance, idEvaluation)
-VALUES
-    ('2023-06-10', 1),
-    ('2023-07-15', 2),
-    ('2023-08-20', 3),
-    ('2023-09-25', 4),
-    ('2023-10-30', 5);
-
-INSERT INTO ESTEVALUE (idEvaluation, idPersonne)
+INSERT INTO ESTEVALUE (idApprenti, idEvaluation)
 VALUES
     (1, 1),
     (2, 2),
